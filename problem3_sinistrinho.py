8eb98b521876dc878b9ad7b308c85cc8e66c62f3
Jacqueline Abreu <jac@jacquelineabreu.me>
2011-08-16 23:07:26 -0300

    Definicao metodo para verificar primalidade e o termino do problema 3 proposto pelo projeto euler

---
+++ problem3_sinistrinho.py
@@ -0,0 +1,22 @@
+from math import *
+
+def isPrime(x):
+	raiz=sqrt(x)
+	for i in range(2, int(raiz)+1):
+		if(x%i==0):
+			return False
+	return True		
+x=600851475143
+for i in range(2, int(sqrt(x))):
+	if(isPrime(i)):
+		while x%i==0:
+			#print i
+			x/=i
+			aux=i
+
+if(x<aux): print aux
+else: print x
+
+	
+	
+	

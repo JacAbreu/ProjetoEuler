ca088e8b70f7194b538ebd1a08d764760ed69e74
Jacqueline Abreu <jac@jacquelineabreu.me>
2011-08-19 21:08:54 -0300

    Soma dos termos pares da sequencia de fibonacci que sejam menores que 4000000

--- problem2.py
+++ problem2.py
@@ -1,15 +1,11 @@
 
 soma_fib=2
+termo1=1
+termo2=2
 
-ter1=1
-ter2=2
-aux=0
-
-while ter2 <= 4000000 :
-	aux=ter1
-	ter1=ter2
-	ter2+=aux
-	if(ter2%2==0):
-		soma_fib+=ter2
+while termo2 <= 4000000 :
+	termo1,termo2=termo2,termo2+termo1 
+	if(termo2%2==0):
+		soma_fib+=termo2
 
 print (soma_fib)
